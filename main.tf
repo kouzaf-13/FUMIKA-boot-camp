9c14dea6d693eaba0eb05ac91609bf15dcf9b263
kouzaf-13 <{Fumika.Kouzaki@ibm.com}>
2025-12-12 14:25:00 +0900

    random

---
+++ main.tf
@@ -0,0 +1,23 @@
+# Random プロバイダを使う宣言
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "random" {}
+
+# ランダム文字列を生成するリソース
+resource "random_string" "fumika-random" {
+  length  = 16   # お好きな文字列長でOK（例：16文字）
+  special = false
+}
+
+# 出力変数
+output "myrandom_output" {
+  value = random_string.fumika-random.result
+}
+
