74e568271efcae61c3760f81bff932fdf8058940
Fumika Kouzaki <fumika.kouzaki@ibm.com>
2025-12-23 20:15:44 +0000

    Fix: Add missing subnetwork resource for VM

--- vpc.tf
+++ vpc.tf
@@ -2,3 +2,10 @@ resource "google_compute_network" "my_vpc" {
   name                    = "fumika-vpc-${random_id.bucket_suffix.hex}"
   auto_create_subnetworks = false
 }
+
+resource "google_compute_subnetwork" "my_subnetwork" {
+  name          = "fumika-subnet"
+  ip_cidr_range = "10.0.1.0/24"
+  region        = "us-central1"
+  network       = google_compute_network.my_vpc.id
+}
