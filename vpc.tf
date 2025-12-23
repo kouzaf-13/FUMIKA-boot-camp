5e09b5ca892d69ef8cbe502731291835b5503b5e
Fumika Kouzaki <fumika.kouzaki@ibm.com>
2025-12-23 20:13:43 +0000

    Fix: Final name alignment for random resource

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,4 @@
 resource "google_compute_network" "my_vpc" {
-  # random_string ではなく、今 main.tf にある random_id を使うように直します
   name                    = "fumika-vpc-${random_id.bucket_suffix.hex}"
   auto_create_subnetworks = false
 }
