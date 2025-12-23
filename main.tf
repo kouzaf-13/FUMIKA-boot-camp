44bc08b1db4328f8fbadb73e44fbc33e182e1f11
Fumika Kouzaki <fumika.kouzaki@ibm.com>
2025-12-23 18:49:58 +0000

    Exercise 5: Finalize cloud migration

--- main.tf
+++ main.tf
@@ -1,32 +1,20 @@
-# Random プロバイダを使う宣言
-terraform {
-  required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
-    }
-  }
-}
-
-provider "random" {}
-
-# ランダム文字列を生成するリソース
-resource "random_string" "fumika-random" {
-  length  = 16   # お好きな文字列長でOK（例：16文字）
-  special = false
-  upper   = false # これを追加して大文字を禁止にします
-}
-
-# 出力変数
-output "myrandom_output" {
-  value = random_string.fumika-random.result
-}
-
 terraform {
   cloud {
     organization = "fumika-kouzaki-terraform"
+
     workspaces {
       name = "FUMIKA-boot-camp"
+    }
+  }
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
     }
   }
 }
