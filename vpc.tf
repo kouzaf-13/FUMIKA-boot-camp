88b0bb0b39bd04cdb836ea6ac073a9866d26322a
Fumika Kouzaki <fumika.kouzaki@ibm.com>
2025-12-23 20:08:29 +0000

    Fix: Link vpc to the correct random_id resource

--- vpc.tf
+++ vpc.tf
@@ -1,18 +1,5 @@
-# VPCの作成
 resource "google_compute_network" "my_vpc" {
-  # random_string ではなく main.tf で定義した random_id を使うように修正します
+  # random_string ではなく、今 main.tf にある random_id を使うように直します
   name                    = "fumika-vpc-${random_id.bucket_suffix.hex}"
   auto_create_subnetworks = false
-}
-
-# 実行後にURLを表示するための設定
-output "vpc_self_link" {
-  value = google_compute_network.my_vpc.self_link
-}
-
-resource "google_compute_subnetwork" "my_subnetwork" {
-  name          = "fumika-subnetwork"
-  ip_cidr_range = "10.0.1.0/24"
-  region        = "us-central1"
-  network       = google_compute_network.my_vpc.id
 }
