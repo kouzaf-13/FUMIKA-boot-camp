322e38ec6927ec69ec503a0ffdd5f7fce1b8abf4
Fumika Kouzaki <fumika.kouzaki@ibm.com>
2025-12-23 19:01:21 +0000

    Fix: Add project ID to provider block

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 terraform {
   cloud {
     organization = "fumika-kouzaki-terraform"
-
     workspaces {
       name = "FUMIKA-boot-camp"
     }
@@ -12,9 +11,21 @@ terraform {
       source  = "hashicorp/google"
       version = "~> 6.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
-    }
   }
+}
+
+provider "google" {
+  # ここにあなたのGCPプロジェクトIDを正確に入れてください
+  project = "hc-54dff18a6bd1444cba92b455f3d" 
+  region  = "us-central1"
+}
+
+# 練習用のリソース（例：バケットなど、今までの演習で使ったもの）
+resource "google_storage_bucket" "test_bucket" {
+  name     = "fumika-test-bucket-${random_id.id.hex}"
+  location = "US"
+}
+
+resource "random_id" "id" {
+  byte_length = 4
 }
